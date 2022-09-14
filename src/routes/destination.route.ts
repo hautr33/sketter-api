5f7592ad643a7caf55f0b62850a8fdd94141fbc1
Hau Tran <hautt33@gmail.com>
2022-09-14 18:58:31 +0700

    update approve/reject/close destination

--- src/routes/destination.route.ts
+++ src/routes/destination.route.ts
@@ -4,7 +4,7 @@ import { requireUser } from "../middlewares/require_user";
 import { standardPipeline } from "../pipes";
 import { restrictTo } from "../controllers/app/auth.controller";
 import { Roles } from "../utils/constant";
-import { approveDestination, createDestination, deleteOneDestination, getAllDestination, getOneDestination, getPendingDestination, getRejectDestination, rejectDestination, updateDestination } from "../controllers/app/destination.controller";
+import { approveDestination, closeDestination, createDestination, deleteOneDestination, getAllDestination, getOneDestination, getPendingDestination, getRejectDestination, rejectDestination, updateDestination } from "../controllers/app/destination.controller";
 
 const router = Router();
 
@@ -28,14 +28,12 @@ router.route('/pending/approve')
     .patch(standardPipeline(restrictTo(Roles["Supplier Manager"]), approveDestination))
 
 router.route('/pending/close')
-    .patch(standardPipeline(restrictTo(Roles["Supplier Manager"]), getRejectDestination))
+    .patch(standardPipeline(restrictTo(Roles["Supplier Manager"]), closeDestination))
 
 router
     .route('/:id')
     .get(standardPipeline(restrictTo(Roles["Supplier Manager"], Roles.Supplier, Roles.Traveler), getOneDestination))
     .patch(standardPipeline(restrictTo(Roles["Supplier Manager"], Roles.Supplier), updateDestination))
     .delete(standardPipeline(restrictTo(Roles["Supplier Manager"], Roles.Supplier), deleteOneDestination))
-
-
 
 export default router;
