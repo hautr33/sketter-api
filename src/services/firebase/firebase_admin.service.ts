d2eed42be742f5fd1e6e0f0450a95ff79ee84651
Hau Tran <hautt33@gmail.com>
2022-10-24 17:04:07 +0700

    update auth google

--- src/services/firebase/firebase_admin.service.ts
+++ src/services/firebase/firebase_admin.service.ts
@@ -4,6 +4,7 @@ import { User } from "../../models/user.model";
 import sequelizeConnection from "../../db/sequelize.db";
 import AppError from "../../utils/app_error";
 import { StatusCodes } from "http-status-codes";
+import { Op } from "sequelize";
 
 export const signUpFirebase = async (user: User): Promise<any> => {
     await sequelizeConnection.transaction(async (create) => {
@@ -20,29 +21,30 @@ export const signUpFirebase = async (user: User): Promise<any> => {
 }
 
 export const loginViaGoogle = async (token: string): Promise<any> => {
-    await sequelizeConnection.transaction(async (save) => {
-        await getAuth()
+    const result = await sequelizeConnection.transaction(async (save) => {
+        const decodedToken = await getAuth()
             .verifyIdToken(token)
-            .then(async (decodedToken: any) => {
-                const user = await User.findOne({ where: { email: decodedToken.email, firebaseID: decodedToken.uid } })
-                if (!user) {
-                    const user = new User()
-                    user.email = decodedToken.email
-                    user.firebaseID = decodedToken.uid
-                    user.name = decodedToken.name
-                    user.avatar = decodedToken.picture
-                    user.authType = Auth.google
-                    user.roleID = Roles.Traveler
-                    user.status = Status.verified
-                    await user.save({ transaction: save })
-                    return user
-                } else if (user.status !== Status.deactivated) {
-                    user.name = decodedToken.name
-                    user.avatar = decodedToken.picture
-                    await user.save({ transaction: save })
-                    return user
-                } else
-                    throw new AppError('Không thể đăng nhập', StatusCodes.BAD_REQUEST)
-            })
+        if (decodedToken && decodedToken.email !== undefined) {
+            const count = await User.count({ where: { email: decodedToken.email, firebaseID: decodedToken.uid } })
+            if (count === 0) {
+                const user = new User()
+                user.email = decodedToken.email
+                user.firebaseID = decodedToken.uid
+                user.name = decodedToken.name
+                decodedToken.picture ? user.avatar = decodedToken.picture : 0
+                user.authType = Auth.google
+                user.roleID = Roles.Traveler
+                user.status = Status.verified
+                await user.save({ transaction: save })
+            } else if (count === 1) {
+                await User.update(decodedToken.picture ? { name: decodedToken.name, avatar: decodedToken.picture } : { name: decodedToken.name }, { where: { email: decodedToken.email, firebaseID: decodedToken.uid }, transaction: save })
+            }
+            const user = await User.findOne({ where: { email: decodedToken.email, firebaseID: decodedToken.uid, status: { [Op.ne]: Status.deactivated } } })
+            if (!user)
+                throw new AppError('Không thể đăng nhập', StatusCodes.BAD_REQUEST)
+            return user
+        } else
+            throw new AppError('Token không hợp lệ', StatusCodes.BAD_REQUEST)
     })
+    return result
 }
