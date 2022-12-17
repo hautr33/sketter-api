e06e318f189d62299936b5cfbfd05477599d4e46
Hau Tran <hautt33@gmail.com>
2022-12-18 00:25:42 +0700

    update refund

--- src/middlewares/voucher.middleware.ts
+++ src/middlewares/voucher.middleware.ts
@@ -22,30 +22,48 @@ export const checkVoucherOrder = async (
             await Transaction.update({ status: Status.failed }, { where: { createdAt: { [Op.lte]: now - 1000 * 60 * 15 }, status: Status.processing }, transaction: order })
         })
         const voucher = await Voucher.findAll({ where: { toDate: { [Op.lte]: (now - 1000 * 3600 * 24) }, status: { [Op.and]: [{ [Op.ne]: Status.draft }, { [Op.ne]: Status.expired }] } } });
-        for (let i = 0; i < voucher.length; i++) {
-            console.log('---------------------');
+        await sequelizeConnection.transaction(async (refundExpired) => {
+            for (let i = 0; i < voucher.length; i++) {
+                console.log('---------------------');
 
-            const detail = await VoucherDetail.findAll({ where: { voucherID: voucher[i].id, status: 'Sold' } })
-            for (let j = 0; j < detail.length; j++) {
-                detail[j].finalPrice = Math.ceil(detail[j].price * (100 - detail[j].refundRate) * (100 - detail[j].commissionRate) / 10) / 1000
-                console.log(detail[j].code + ' - ' + detail[j].finalPrice);
-                const transaction = new Transaction()
-                transaction.voucherDetailID = detail[j].id
-                transaction.travelerID = detail[j].travelerID as string
-                var date = new Date();
-                var format = require('date-format');
-                var orderId = format('hhmmss', date);
-                transaction.orderID = orderId
-                transaction.orderInfo = 'Refund ' + detail[j].code
-                transaction.amount = Math.ceil(detail[j].price * (100 - detail[j].refundRate))
-                transaction.vnpTransactionNo = (Date.now() + '').substring(5)
-                transaction.vnpTransactionStatus = '00'
-                transaction.transactionType = 'Refund'
-                transaction.status = 'Success'
-                // await transaction.save({ transaction: confirm })
+                const detail = await VoucherDetail.findAll({ where: { voucherID: voucher[i].id, status: 'Sold' } })
+                for (let j = 0; j < detail.length; j++) {
+                    detail[j].finalPrice = Math.ceil(detail[j].price * (100 - detail[j].refundRate) * (100 - detail[j].commissionRate) / 10) / 1000
+                    detail[j].status = 'Refunded'
+
+                    const refund = new Transaction()
+                    refund.voucherDetailID = detail[j].id
+                    refund.travelerID = detail[j].travelerID as string
+                    var format = require('date-format')
+                    var orderId = format('hhmmss', new Date())
+                    refund.orderID = orderId
+                    refund.orderInfo = 'Refund ' + detail[j].code
+                    refund.amount = Math.ceil(detail[j].price * (100 - detail[j].refundRate)) * 10
+                    refund.vnpTransactionNo = (Date.now() + '').substring(5)
+                    refund.vnpTransactionStatus = '00'
+                    refund.transactionType = 'Refund'
+                    refund.status = 'Success'
+
+                    const income = new Transaction()
+                    income.voucherDetailID = detail[j].id
+                    income.travelerID = detail[j].travelerID as string
+                    orderId = format('hhmmss', new Date())
+                    income.orderID = orderId
+                    income.orderInfo = 'Income ' + detail[j].code
+                    income.amount = (detail[j].finalPrice ?? 0) * 1000
+                    income.vnpTransactionNo = (Date.now() + '').substring(5)
+                    income.vnpTransactionStatus = '00'
+                    income.transactionType = 'Income'
+                    income.status = 'Success'
+
+                    console.log(detail[j].code + ' - ' + refund.amount + ' - ' + income.amount)
+
+                    await refund.save({ transaction: refundExpired })
+                    await income.save({ transaction: refundExpired })
+                    await detail[j].save({ transaction: refundExpired })
+                }
             }
-        }
-
+        })
         next();
     } catch (err: any) {
         next(err);
